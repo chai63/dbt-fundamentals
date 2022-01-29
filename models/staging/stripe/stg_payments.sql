5e49be4a04e5a3e519585b63bf4ca118aa478b3b
kulkarnichaitanya623@gmail.com <kulkarnichaitanya623@gmail.com>
2022-01-29 04:27:16 +0000

    macros and other shenanigans

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -2,7 +2,9 @@ with payment as (
 select 
     id          as payment_id
     , orderid   as order_id
-    , amount
+    , {{ cents_to_dollars('amount','4') }} as amount
+    , status
+    , paymentmethod as payment_method
 from {{ source('stripe', 'payment') }}
 )
 
